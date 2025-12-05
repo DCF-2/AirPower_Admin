583b61f7fe7edb49b3584fc9f47dc9017687c156
Willian Santos <willjsan@motorola.com>
2025-12-05 15:51:06 -0300

    feat(ui): Refatora NotificationCard e alinha layou
    •Refatoração de Layout: O layout do card de notificação
    foi completamente reestruturado usando ConstraintLayout
    para um posicionamento mais preciso e eficiente dos elementos.
    •Alinhamento Visual: Todos os campos (Assunto, Origem,
    Severidade, Tipo e Horário) agora estão alinhados em
    uma grade vertical clara, melhorando drasticamente a
    legibilidade e a aparência profissional.
    •Componentes Auxiliares Removidos: Funções auxiliares
    como GetPaddingStart() e a estrutura complexa de Row e
    Column aninhados foram removidas em favor da
    simplicidade e performance do ConstraintLayout.

--- app/src/main/java/com/ifpe/edu/br/view/ui/screens/NotificationCenterScreen.kt
+++ app/src/main/java/com/ifpe/edu/br/view/ui/screens/NotificationCenterScreen.kt
@@ -1,21 +1,38 @@
 package com.ifpe.edu.br.view.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.ifpe.edu.br.common.CommonConstants
+import com.ifpe.edu.br.common.components.CustomCard
 import com.ifpe.edu.br.common.components.CustomColumn
+import com.ifpe.edu.br.common.components.CustomText
+import com.ifpe.edu.br.common.ui.theme.cardBackgroundGradientDark
+import com.ifpe.edu.br.common.ui.theme.cardBackgroundGradientLight
+import com.ifpe.edu.br.common.ui.theme.cardCornerRadius
 import com.ifpe.edu.br.model.Constants
 import com.ifpe.edu.br.model.repository.remote.dto.AirPowerNotificationItem
 import com.ifpe.edu.br.view.ui.components.NotificationCard
+import com.ifpe.edu.br.view.ui.theme.tb_primary_light
 import com.ifpe.edu.br.viewmodel.AirPowerViewModel
 
 /*
@@ -33,12 +50,46 @@ fun NotificationCenterScreen(
         modifier = Modifier.fillMaxSize(),
         alignmentStrategy = CommonConstants.Ui.ALIGNMENT_TOP,
         layouts = listOf {
-            NotificationGrid(
-                notificationSet = notification.value,
-                viewModel = mainViewModel
-            )
+            if (notification.value.isEmpty()) {
+                EmptyStateNotificationCard()
+            } else {
+                NotificationGrid(
+                    notificationSet = notification.value,
+                    viewModel = mainViewModel
+                )
+            }
         }
     )
+}
+
+@Composable
+fun EmptyStateNotificationCard() {
+    Box(
+        modifier = Modifier
+            .background(Color.Transparent)
+    ) {
+        CustomCard(
+            modifier = Modifier
+                .clip(RoundedCornerShape(cardCornerRadius))
+                .fillMaxSize()
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = if (isSystemInDarkTheme()) cardBackgroundGradientDark else cardBackgroundGradientLight,
+                        start = Offset(0f, 0f),
+                        end = Offset(1000f, 1000f)
+                    )
+                ),
+            layouts = listOf {
+                Spacer(modifier = Modifier.padding(vertical = 65.dp))
+                CustomText(
+                    color = tb_primary_light,
+                    text = "Você não possui notificações",
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.padding(vertical = 65.dp))
+            }
+        )
+    }
 }
 
 @Composable
